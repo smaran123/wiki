31b43dfbba8945343568412bcb2fd8ac0282b3b4
smaran123 <smaranreddy123@gmail.com>
2015-06-06 21:04:20 +0530

    crud operations of article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+
+	before_action :set_article, :only=>[:show,:edit,:update,:destroy]
+
+	def index
+		@articles = Article.all.order("created_at ASC")
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render "new"
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render "edit"
+		end
+	end
+
+	def show
+	end 
+
+	def destroy
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def set_article
+		@article=Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit!
+	end
+
+
+end
